79297df991d3a035181bd9f604d42384d4a92fb1
dag <LunaPg@users.noreply.github.com>
2020-01-16 22:48:27 -0500

    Setup (#3)
    * Added mocha, sinon, chai, typescript, gbfs-client packages
    
    * Added ts types, nyc config, nyc, eslint + config, prettier + config
    
    * Added mocha, sinon, chai, typescript, gbfs-client packages
    
    * Added ts types, nyc config, nyc, eslint + config, prettier + config
    
    * feat(setup): add package.lock
    
    * feat(setup): add dummy ts file
    
    * chore(setup): add empty mocha.opts
    
    * chore(setup): add empty mocha.opts + dummy ts
    
    * feat(setup): added tests filder
    
    * fix(test): added proper mocha config + ts-node
    
    * add push notification modules + ts types
    
    Co-authored-by: agnandiane <agnandiane@gmail.com>

---
+++ src/tests/index.spec.ts
@@ -0,0 +1,7 @@
+import {assert} from 'chai';
+describe('first', () => {
+    it('should be a dummy test', () => {
+        //expect('string').to.be.string;
+        assert (true, 'Dummy test kill me !');
+    });
+});
