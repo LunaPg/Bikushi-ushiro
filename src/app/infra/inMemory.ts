a9af3cbbf5e460cb1dd197b830e0e21e5e41d5d8
Luna <malalunapg@gmail.com>
2020-02-06 23:12:56 -0500

    fix(inMemory): lint + remove shame test :P

--- src/app/infra/inMemory.ts
+++ src/app/infra/inMemory.ts
@@ -1,18 +1,15 @@
-// import { Station } from "../../domain/station/entity";
 import {Projection} from './projection';
 import { StationInfo } from 'gbfs-client/lib/types';
 
 export class inMemory implements Projection{
-    protected stations = new Map<string, StationInfo>();
+    public stations = new Map<string, StationInfo>();
 
-    // constructor();
+    public add(station: StationInfo)  { 
+      this.stations.set(station.station_id, station);
+      return true;  
+    }
 
-    public add(station: StationInfo)  {
-      try{
-        this.stations.set(station.station_id, station);
-        return true;
-      }catch(error){
-        throw error;
-      } 
+    public get(stationId: string): StationInfo | undefined{
+      return this.stations.get(stationId);
     }
 }
--- src/tests/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import {assert} from 'chai';
-describe('first', () => {
-    it('should be a dummy test', () => {
-        //expect('string').to.be.string;
-        assert (true, 'Dummy test kill me !');
-    });
-});
