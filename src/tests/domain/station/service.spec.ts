dc98ad718bf8272946d1b2802afac91b410c263d
Luna <malalunapg@gmail.com>
2020-02-06 23:04:08 -0500

    feat(test): add infra test, cleaning service test

--- src/tests/domain/station/service.spec.ts
+++ src/tests/domain/station/service.spec.ts
@@ -1,22 +1,30 @@
-import {  expect} from 'chai';
-import {sandbox as sinonSandbox} from 'sinon';
-// import GbfsClient from 'gbfs-client';
-import {gbfsStub, inMemoryStub } from "../../stubs";
+import {  expect } from 'chai';
+import { createSandbox } from 'sinon';
+import { gbfsStub, inMemoryStub } from "../../stubs";
 
 import { StationInfo } from 'gbfs-client/lib/types';
 import faker from "faker";
 
 import { stationService } from '../../../domain/station/service';
-// import { fail } from 'assert';
 
 describe('service', () => {
-  const sandbox = sinonSandbox.create();
-  
-  let StationService = new stationService(gbfsStub as any , inMemoryStub as any );
+  const sandbox = createSandbox();
+  let memoryStub : any;
+  let gbfsclientStub: any
+  let StationService: stationService;
+
+  beforeEach( ()=> {
+    memoryStub = inMemoryStub(sandbox);
+    gbfsclientStub = gbfsStub(sandbox)
+    StationService = new stationService(gbfsclientStub as any , memoryStub as any );
+  });
 
   afterEach( ()=> {
+    memoryStub = null;
+    gbfsclientStub = null;
     sandbox.restore();
-  }) 
+  });
+
   describe('add', () => {
     it('should return a station successfully',async () => {
       const stationInfo : StationInfo = {
@@ -35,21 +43,21 @@ describe('service', () => {
       // Have to return ArrayValue because ts
       // method overloading return different type of
       // pormise, could not test it with ts.
-      gbfsStub.stationInfo.withArgs().returns(Promise.resolve([stationInfo]));
-      inMemoryStub.add.returns(true);
+      gbfsclientStub.stationInfo.withArgs().returns(Promise.resolve([stationInfo]));
+      memoryStub.add.returns(true);
       const res = await StationService.add("1");
       expect(res).to.equal(true);
     });
+
     it('should throw when statinInfo fail', () => {
-      gbfsStub.stationInfo.withArgs().returns(Promise.reject(new Error('fails')));
+      gbfsclientStub.stationInfo.withArgs().returns(Promise.reject(new Error('fails')));
       // to.be.rejectedWith have issue with ts, 
       // switches to standard promises
        return StationService.add("1").then(value => {
         expect.fail( value, 'Should have vailed, instead returns a value');
        }).catch(e => {
          expect(e).to.be.instanceOf(Error);
-       })
+       });
     });
-
   });
 });
