ec5dee480f65814e70c51bbacfe03415f2a2c7f7
Luna <malalunapg@gmail.com>
2020-02-08 22:19:18 -0500

    fix (service): add manage to insert both array and sinble station)
    See : https://github.com/mnkhouri/node-gbfs-client/pull/5

--- src/domain/station/service.ts
+++ src/domain/station/service.ts
@@ -7,16 +7,20 @@ export class stationService{
 
   async add(stationId: string){
     try {
-      const station: StationInfo = await this.Gbgs.stationInfo(stationId);
+      const station: StationInfo | StationInfo[] = await this.Gbgs.stationInfo(stationId);
       console.log(station);
-      if (!station) {
+      if (!station ) {
         throw new Error('[404] not Found');
       }
-      this.projection.add(station);
+      if (station instanceof Array){
+        station.map((one) =>  this.projection.add(one));
+      }
+      else {this.projection.add(station);}
+      
       return true;
     } catch(e) {
       throw new Error (e);
     }
   }
 
-}   
+}  
